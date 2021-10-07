cc20f782beb9d7e17529ad400b4373a9a2e66717
junyeong-b2n2w <>
2021-10-08 00:03:13 +0900

    안드로이드 글로벌

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {WebView} from 'react-native-webview';
+import messaging from '@react-native-firebase/messaging';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+const domain = 'http://www.naver.com/';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      indexPage: {uri: domain + '?ver=1'},
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <WebView
+          style={styles.webview}
+          source={this.state.indexPage}
+          originWhitelist={['*']}
+          ref={webview => (this.appWebview = webview)}
+          javaScriptEnabled={true}
+          useWebKit={true}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+  },
+  webview: {
+    flex: 1,
+  },
+});
---
+++ ios/Pods/Target Support Files/Pods-osolapp-osolappTests/Pods-osolapp-osolappTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_osolapp_osolappTests : NSObject
+@end
+@implementation PodsDummy_Pods_osolapp_osolappTests
+@end
